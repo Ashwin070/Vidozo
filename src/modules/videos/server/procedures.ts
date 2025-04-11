c42928586cafe215d5e1ea8a0257275421874029
Ashwin Shaji <ashwinshaji154@gmail.com>
2025-04-11 16:27:05 +0530

    Changed the schema to add video description, visibility, thumbnail and preview. Added frontend changes to studio.

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -4,27 +4,37 @@ import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
-    create: protectedProcedure.mutation(async ({ctx}) => {
-        const {id: userId} = ctx.user;
-        const upload = await mux.video.uploads.create({
-            new_asset_settings: {
-                passthrough: userId,
-                playback_policy: ["public"],
-            },
-            cors_origin: "*", //TODO
-        })
-        const [video] = await db
-            .insert(videos)
-            .values({
-                userId,
-                title: "Untitled",
-                muxStatus: "waiting",
-                muxUploadId: upload.id
-            })
-            .returning();
-        return {
-            video: video,
-            url: upload.url
-        }
-    })
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    const { id: userId } = ctx.user;
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        input: [
+          {
+            generated_subtitles: [
+              {
+                language_code: "en",
+                name: "English",
+              },
+            ],
+          },
+        ],
+      },
+      cors_origin: "*", //TODO
+    });
+    const [video] = await db
+      .insert(videos)
+      .values({
+        userId,
+        title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
+      })
+      .returning();
+    return {
+      video: video,
+      url: upload.url,
+    };
+  }),
 });
